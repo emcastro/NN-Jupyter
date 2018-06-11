0c3c982efe7d43c211d33a3d711cb59cb55b6015
Emmanuel Castro <emmanuel.castro@laposte.net>
2018-06-12 00:10:13 +0200

    Amélioration de JSON Undefined

--- json_object.py
+++ json_object.py
@@ -1,23 +1,25 @@
 import json
+from typing import Any
 
 class JsonUndefined:
-
     def __str__(self):
         return 'undefined'
 
+    def __bool__(self):
+        return False
+
     def __init__(self):
-        if JsonUndefined.INSTANCE:
+        if JsonUndefined.INSTANCE != None:
             raise Error('Instance already initalized')
-        JsonUndefined.INSTANCE = self
 
-    INSTANCE = None
+    INSTANCE: Any = None
 
-JsonUndefined()
+JsonUndefined.INSTANCE = JsonUndefined()
 
 class Json(dict):
     """Dict that can use Javascript like JSON"""
 
-    undefined = JsonUndefined.INSTANCE
+    undefined: JsonUndefined = JsonUndefined.INSTANCE
 
     def __setattr__(self, name, value):
         if value == Json.undefined:
@@ -34,20 +36,37 @@ class Json(dict):
     def __delattr__(self, name):
         del self[name]
 
-    def __str__(self):
+    def __repr__(self):
         return json.dumps(self, indent=2)
+
+    @staticmethod
+    def loads(s: str):
+        return json.loads(s, object_hook=Json)
 
 
 if __name__ == '__main__':
+    import re
+
+    # Checking is None and Json.undefined have JS boolean semantics
+    assert not None
+    assert not Json.undefined
+
     x = Json(a=3)
     x.b = 'c'
-    assert x['b'] == 'c'
-    print(x)
-    x.truc = None
+    assert 'b' in x
+    assert x['b'] == 'c'  # x.b ==> x['b']
+    assert re.sub('\s', '', str(x)) == '{"a":3,"b":"c"}'
+
+    x.none = None
+    assert x['none'] == None  # None => null
+
     del x.a
-    print(x)
-    print('a' in x)
-    print('b' in x)
-    print(x.a)
-    x.truc = Json.undefined
-    print(x)
+    assert 'a' not in x
+    assert x.a == Json.undefined  # missing return Json.undefined
+    assert x.never_used == Json.undefined
+    assert re.sub('\s', '', str(x)) == '{"b":"c","none":null}'
+
+    x.none = Json.undefined  # setting undefined removes
+    assert 'none' not in x
+    y = Json.loads(str(x))
+    assert x == y
